7d2ac4da2be93231f8dd183cfe3465c8f5e9ea9a
bethojunior <hgomescostajr@gmail.com>
2020-11-11 14:41:15 -0300

    pesquisa de satisfação tuttishjop

--- email.php
+++ email.php
@@ -1,7 +1,12 @@
 
 <?php
 
-$destino = "contato@madgic.com.br";
+$config  = "MIME-Version: 1.1\r\n";
+$config .= "Content-type: text/plain; charset=UTF-8\r\n";
+$config .= "From: contato@tuttishop.com.br\r\n"; // remetente
+$config .= "Return-Path: contato@tuttishop.com.br\r\n"; // return-path
+
+$destino = "hgomescostajr@gmail.com";
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $texto = $_POST['texto'];
@@ -9,8 +14,8 @@ $headers = "Enviado por : " . $nome . "\n";
 $mensagem = "Enviado por : " . $nome . "\n"."Email: ".$email."\n"."Motivo :".$texto;
 
 if(mail($destino, $headers , $mensagem)){
-    echo true;
+    echo 'enviado';
 }else{
-    echo false;
+    echo 'erro ao enviar';
 }
 ?>
